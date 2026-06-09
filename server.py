45fc80c44bb64f8d9afd111cf06a857e8a25aba2
Rajasekar Bathiri Narayanan <email2raja.b@gmail.com>
2026-06-09 14:07:35 -0400

    Add EmotionDetection package

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
+from sentiment_analysis import sentiment_analyzer
 
-#Initiate the flask app : TODO
+sentiment_analyzer("I love this new technology.")
+
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
